481539e03704ec8a30469fb9ac337d85dcfc1e13
Anis B <abouhdaoui@gmail.com>
2020-06-05 15:58:47 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/com/itcode/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.itcode.sfgpetclinic.model;
+
+/**
+ * Created by abouhdaoui on 05/06/2020
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/itcode/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.itcode.sfgpetclinic.model;
+
+/**
+ * Created by abouhdaoui on 05/06/2020
+ */
+public class Vet extends Person {
+}
